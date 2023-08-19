3030f33af68bab6b4b0cc9c06e38c4c1890adb35
Ashish <ashishm.programmics@gmail.com>
2023-08-19 11:12:58 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
-const Url = 'mongodb://localhost/assignment_2'
+// const Url = 'mongodb://localhost/assignment_2'
+const Url = 'mongodb+srv://ashish_protech:<123456>@firstcluster.yrutbmz.mongodb.net/assignment-2?retryWrites=true&w=majority'
 
 exports.connect = () => {
     mongoose.connect(Url)
